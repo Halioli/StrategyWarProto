146a3615effa6a40bfe97134216e3d175e20d586
Aniol <aniolbarnadat@gmail.com>
2022-07-15 18:31:05 +0200

    Created base movement

---
+++ Assets/Code/NavMeshClickController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshClickController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                GetComponent<NavMeshAgent>().SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
---
+++ Assets/Code/PathCreator.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PathCreator : MonoBehaviour
+{
+    private static float MIN_DISTANCE = 1f;
+
+    private LineRenderer lineRenderer;
+    private List<Vector3> points = new List<Vector3>();
+
+    public Action<IEnumerable<Vector3>> OnNewPathCreated = delegate { };
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        // Click
+        if (Input.GetButtonDown("Fire1"))
+        {
+            points.Clear();
+            lineRenderer.positionCount = 0;
+        }
+
+
+        // Held
+        if (Input.GetButton("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                if (DistanceToLastPoint(hitInfo.point) > MIN_DISTANCE)
+                {
+                    points.Add(hitInfo.point);
+
+                    lineRenderer.positionCount = points.Count;
+                    lineRenderer.SetPositions(points.ToArray());
+                }
+            }
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            OnNewPathCreated(points);
+        }
+    }
+
+    private float DistanceToLastPoint(Vector3 point)
+    {
+        if (!points.Any())
+            return Mathf.Infinity;
+
+        return Vector3.Distance(points[points.Count - 1], point);
+    }
+}
